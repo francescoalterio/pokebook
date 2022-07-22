abb099bc6a45aed1c8e8dfafc444d406e1373666
Francesco Alterio <francescoalteriog@gmail.com>
2022-07-22 10:17:02 -0400

    add admob in my app

--- App.js
+++ App.js
@@ -6,7 +6,17 @@ import Screens from "./src/Screens";
 
 import store from "./src/store";
 
+import mobileAds from "react-native-google-mobile-ads";
+import { AppOpenAd, TestIds } from "react-native-google-mobile-ads";
+
 export default function App() {
+  mobileAds()
+    .initialize()
+    .then((adapterStatuses) => {
+      // Initialization complete!
+      AppOpenAd.createForAdRequest(TestIds.APP_OPEN);
+    });
+
   return (
     <Provider store={store}>
       <SafeAreaView style={styles.container}>
